eb4f62b0c48dd396a37bffaac35715497d5e4ec6
Xinjiang Shao <shaoxinjiang@gmail.com>
2013-07-23 22:22:51 -0500

    Added script to download and install mongocxxdriver

---
+++ buildscript/install_mongocxx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf mongo-cxx-driver-v2.4
+rm -rf mongodb-linux-x86_64-v2.4-latest.tgz
+wget http://downloads.mongodb.org/cxx-driver/mongodb-linux-x86_64-v2.4-latest.tgz
+tar xzf mongodb-linux-x86_64-v2.4-latest.tgz
+cd mongo-cxx-driver-v2.4
+sudo rm .sconsign.dblite
+scons
+sudo scons install
+sudo chmod 755 -R /usr/local/include/mongo/*
+sudo chmod 755 -R /usr/local/lib/libmongo*
